3dc59c1103b10b53c71d04de9481095800247838
Fine <fine_567@hotmail.com>
2024-01-28 19:59:36 -0600

    second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,35 +5,23 @@ import { Page404Component } from './shared/components/page404/page404.component'
 
 export const routes: Routes = [
     {
-        path:'website',
-        loadComponent: ()=> import('./modules/website/website.component'),
-        children: [
-            {
-                path:'home',
-                title:'Home',
-                loadComponent: ()=> import('./modules/website/pages/home/home.component')
-            },
-            {   path:'', redirectTo:'home',pathMatch:'full'},
-        ]
-    },
-    {
         path:'auth',
         loadComponent: ()=> import ('./modules/auth/auth.component'),
         children: [
             {
                 path:'login',
                 title:'Login',
-                loadComponent: ()=> import('./modules/auth/pages/login/login.component')
+                // loadComponent: ()=> import('./modules/auth/pages/login/login.component')
             },
             {
                 path:'forgot-password',
                 title:'Forgot Password',
-                loadComponent: ()=> import('./modules/auth/pages/forgot-password/forgot-password.component')
+                // loadComponent: ()=> import('./modules/auth/pages/forgot-password/forgot-password.component')
             },
             {
                 path:'create-account',
                 title:'Create Account',
-                loadComponent: ()=> import('./modules/auth/pages/create-account/create-account.component')
+                // loadComponent: ()=> import('./modules/auth/pages/create-account/create-account.component')
             },
             {   path:'', redirectTo:'login',pathMatch:'full'},
         ]
@@ -43,14 +31,9 @@ export const routes: Routes = [
         loadComponent: ()=> import ('./modules/dashboard/dashboard.component'),
         children:[
             {
-                path:'profile',
-                title:'Profile',
-                loadComponent: ()=> import('./modules/dashboard/pages/profile/profile.component')
-            },
-            {
                 path:'main',
                 title:'Main',
-                loadComponent: ()=> import('./modules/dashboard/pages/main/main.component')
+                // loadComponent: ()=> import('./modules/dashboard/pages/main/main.component')
             },
             {   path:'', redirectTo:'main',pathMatch:'full'},
         ]
--- src/app/modules/auth/auth.component.ts
+++ src/app/modules/auth/auth.component.ts
@@ -2,18 +2,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 // * Components
-import { ChangeModeComponent } from '@shared/components/change-mode/change-mode.component';
-import { VersionComponent } from '@shared/components/version/version.component';
 import { LocalStorageService } from '../../core/services/localstorage.service';
-import { ChangeLangComponent } from '@shared/components/change-lang/change-lang.component';
+import { VersionComponent } from '@shared/components/version/version.component';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [ 
     RouterModule, 
-    ChangeModeComponent,
-    ChangeLangComponent,
     VersionComponent,
   ],
   templateUrl: './auth.component.html',
